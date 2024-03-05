d5ac62908b7582055ffe9430e92398aca8ccc980
meashna <cs25ashn@kmeacollege.ac.in>
2024-03-06 01:24:54 +0530

    first commit

--- app.js
+++ app.js
@@ -16,15 +16,37 @@ app.use(cors());
 
 // app.use(
 //   cors({
-//     origin: "https://merntodo-backend.vercel.app/",
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//     preflightContinue: true,
+//     optionsSuccessStatus: 200,
+//     maxAge: 3600,
 //   })
 // );
 
-app.use(
-  cors({
-    origin: "https://merntodo-frontend.vercel.app/",
-  })
-);
+const corsOptions = {
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      "http://localhost:5173",
+      "https://merntodo-frontend.vercel.app",
+    ];
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS policy violation"));
+    }
+  },
+  credentials: true, // to support cookies
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  maxAge: 3600,
+};
+
+app.use(cors(corsOptions));
 
 app.use("/api/v1", authRoutes);
 app.use("/api/v2", listRoutes);
@@ -34,7 +56,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 });
 
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
